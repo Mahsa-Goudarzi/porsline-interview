05f5610f33ee120196f8e7d8c4f1e84353dbddde
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2023-10-09 17:47:51 +0330

    move the handle letter click functions to context

--- src/components/keyboard/keyboard-detail/Key.js
+++ src/components/keyboard/keyboard-detail/Key.js
@@ -5,25 +5,15 @@ import WordContext from "../../../store/words-context";
 import classes from "./Key.module.css";
 
 export default function Key({ value }) {
-  const { guesses, setGuesses, currentGuess, setCurrGuess } =
-    useContext(WordContext);
+  const { onClear, onEnter, onClickLetter } = useContext(WordContext);
 
   const keyClickHandler = () => {
     if (value === "Enter") {
-      if (currentGuess.letterPos !== 5) return;
-      setCurrGuess({ attempt: currentGuess.attempt + 1, letterPos: 0 });
+      onEnter();
     } else if (value === "Clear") {
-      if (currentGuess.letterPos === 0) return;
-      const newBoard = [...guesses];
-      newBoard[currentGuess.attempt][currentGuess.letterPos - 1] = "";
-      setGuesses(newBoard);
-      setCurrGuess({ ...currentGuess, letterPos: currentGuess.letterPos - 1 });
+      onClear();
     } else {
-      if (currentGuess.letterPos > 4) return;
-      const newBoard = [...guesses];
-      newBoard[currentGuess.attempt][currentGuess.letterPos] = value;
-      setGuesses(newBoard);
-      setCurrGuess({ ...currentGuess, letterPos: currentGuess.letterPos + 1 });
+      onClickLetter(value);
     }
   };
 
--- src/store/words-context.js
+++ src/store/words-context.js
@@ -17,26 +17,45 @@ export const WordContext = React.createContext({
   currentGuess: null,
   setGuesses: (newBoard) => {},
   setCurrGuess: (newGuess) => {},
+  onClear: () => {},
+  onEnter: () => {},
+  onClickLetter: (value) => {},
 });
 
 export const WordsContextProvider = (props) => {
   const [board, setBoard] = useState(initialBoard);
   const [currGuess, setCurrGuess] = useState(initialGuess);
 
-  const setGuessesHandler = (newBoard) => {
+  const handleClear = () => {
+    if (currGuess.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currGuess.attempt][currGuess.letterPos - 1] = "";
     setBoard(newBoard);
+    setCurrGuess({ ...currGuess, letterPos: currGuess.letterPos - 1 });
   };
 
-  const setCurrGuessHandler = (newGuess) => {
-    setCurrGuess(newGuess);
+  const handleEnter = () => {
+    if (currGuess.letterPos !== 5) return;
+    setCurrGuess({ attempt: currGuess.attempt + 1, letterPos: 0 });
+  };
+
+  const handleClickLetter = (value) => {
+    if (currGuess.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currGuess.attempt][currGuess.letterPos] = value;
+    setBoard(newBoard);
+    setCurrGuess({ ...currGuess, letterPos: currGuess.letterPos + 1 });
   };
 
   const contextValue = {
     word: "rainy",
     guesses: board,
     currentGuess: currGuess,
-    setGuesses: setGuessesHandler,
-    setCurrGuess: setCurrGuessHandler,
+    setGuesses: setBoard,
+    setCurrGuess: setCurrGuess,
+    onClear: handleClear,
+    onEnter: handleEnter,
+    onClickLetter: handleClickLetter,
   };
 
   return (

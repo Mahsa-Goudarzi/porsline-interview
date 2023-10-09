6805a5026bc5644dabfb5542e77c94db7526bcaf
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2023-10-09 17:23:56 +0330

    added a function to enter different letters of a guess

--- src/components/keyboard/keyboard-detail/Key.js
+++ src/components/keyboard/keyboard-detail/Key.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Key.module.css";
+import WordContext from "../../../store/words-context";
 
 export default function Key({ value }) {
-  return <div className={classes.key}>{value}</div>;
+  const { guesses, setGuesses, currentGuess, setCurrGuess } =
+    useContext(WordContext);
+
+  const keyClickHandler = () => {
+    const newBoard = [...guesses];
+    newBoard[currentGuess.attempt][currentGuess.letterPos] = value;
+    setGuesses(newBoard);
+    setCurrGuess(currentGuess.attempt, currentGuess.letterPos + 1);
+  };
+
+  return (
+    <div className={classes.key} onClick={keyClickHandler}>
+      {value}
+    </div>
+  );
 }
--- src/store/words-context.js
+++ src/store/words-context.js
@@ -9,23 +9,34 @@ const initialBoard = [
   ["", "", "", "", ""],
 ];
 
+const initialGuess = { attempt: 0, letterPos: 0 };
+
 export const WordContext = React.createContext({
   word: "",
   guesses: null,
+  currentGuess: null,
   setGuesses: (newBoard) => {},
+  setCurrGuess: (newAtt, newPos) => {},
 });
 
 export const WordsContextProvider = (props) => {
   const [board, setBoard] = useState(initialBoard);
+  const [currGuess, setCurrGuess] = useState(initialGuess);
 
   const setGuessesHandler = (newBoard) => {
     setBoard(newBoard);
   };
 
+  const setCurrGuessHandler = (att, pos) => {
+    setCurrGuess({ attempt: att, letterPos: pos });
+  };
+
   const contextValue = {
     word: "rainy",
     guesses: board,
+    currentGuess: currGuess,
     setGuesses: setGuessesHandler,
+    setCurrGuess: setCurrGuessHandler,
   };
 
   return (
